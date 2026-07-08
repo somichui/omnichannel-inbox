3ec326af306a56b0d10e7c7b34aef8f944e7fbe7
somichui <somichuizimik@gmail.com>
2026-07-08 07:17:42 +0530

    Fix backend dist folder structure and gitignore

--- backend/dist/src/telegram/telegram.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { TelegramService } from './telegram.service';
-export declare class TelegramController {
-    private readonly telegramService;
-    private readonly logger;
-    constructor(telegramService: TelegramService);
-    handleWebhook(payload: any): Promise<string>;
-}
